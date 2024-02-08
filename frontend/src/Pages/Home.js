a7164cb4d381edebccc463df34e94c8cdd69ad56
Sowmya <stanikon@asu.edu>
2024-02-08 13:34:02 -0700

    reverting the changes in HomePage

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -3,13 +3,12 @@ import Btn from "../Components/Button/Btn";
 import TextBox from "../Components/TextBox/TextBox";
 import { getTestData } from "../Services";
 import Login from "../Components/Login/Login";
-import Signup1 from "../Components/Signup1/Signup1"
-//import Signup2 from "../Components/Signup2/Signup2";
 
 class Home extends Component {
   constructor() {
     super();
     this.state = {
+      // State variables here
       backendResponse: "",
     };
   }
@@ -30,18 +29,21 @@ class Home extends Component {
   render() {
     const { backendResponse } = this.state;
     return (
-     
-
-      <div style={{ backgroundColor: "#f0f0f0", textAlign: "center", padding: "50px", height: "100vh", position: "relative" }}>
-        <div style={{ backgroundColor: "lightblue", padding: "10px", marginBottom: "20px" }}>
-          <h1 style={{ margin: 0, fontFamily: 'Sharp Sans, sans-serif' }}>Game Night Recommender</h1>
-        </div>
-        <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", backgroundColor: "white", textAlign: "center", padding: "10px", width: "80vh", height: "50vh" }}>
-          <Login /> 
-          {/*<Signup1/>*/} 
-        </div>
-      </div> 
-   /*<Signup2/>*/
+      <div>
+        {/* <h1>Game-Night-Recommendation-System</h1>
+        <Btn
+          label={"Material UI Button"}
+          onClick={() => {
+            alert("Button Clicked");
+          }}
+        />
+        <TextBox label={"Sample text box"} />
+        {backendResponse !== "" && (
+          <p>Response from backend : {backendResponse.message}</p>
+        )}
+         */}
+         <Login />
+      </div>
     );
   }
 }
