2e1b9bb6158e220dfeabc02063f5ec2f682310f3
Sowmya <stanikon@asu.edu>
2024-02-08 12:39:01 -0700

    I've added SignUp button in the Login UI

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import TextBox from "../TextBox/TextBox";
-import { Container } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import Btn from "../Button/Btn";
 import Text from "../Typography/Text";
 
@@ -13,17 +13,23 @@ const Login = () => {
     console.log("Password:", password);
   };
 
+  const handleSignUp = () => {
+    // Handle SignUp action here, navigate to SignUp page or show SignUp form
+    console.log("Navigate to SignUp page or show SignUp form");
+  };
+
   return (
     <Container maxWidth="sm">
-      <Text variant="h4" gutterBottom={true} label={"Login"} />
- 
+      <Grid container justifyContent="space-between" alignItems="center" marginBottom={2}>
+        <Text variant="h4" gutterBottom={true} label={"Login"} />
+        <Btn label="Sign Up" onClick={handleSignUp} />
+      </Grid>
       <TextBox
         label="Username"
         value={username}
         fullWidth={true}
         onChange={(e) => setUsername(e.target.value)}
       />
-
       <TextBox
         label="Password"
         type="password"
@@ -31,7 +37,6 @@ const Login = () => {
         fullWidth={true}
         onChange={(e) => setPassword(e.target.value)}
       />
-
       <Btn fullWidth={true} label={"Submit"} onClick={handleLogin} />
     </Container>
   );
