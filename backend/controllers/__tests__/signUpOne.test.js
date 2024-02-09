6ec6b2b413100eaabce349f0febc10eba69b189c
Diya Roshan Sanghvi <diyaroshansanghvi@diyas-air.dhcp.asu.edu>
2024-02-09 16:44:57 -0700

    added user step 1 signup API, tests, routes

---
+++ backend/controllers/__tests__/signUpOne.test.js
@@ -0,0 +1,35 @@
+const request = require("supertest");
+const app = require("../../app");
+const User = require("../../models/User/userModal");
+const mongoose = require("mongoose");
+
+describe("Sign up step one page testing", () => {
+  const paramBody = {
+    name: "SER test case admin",
+    email: "sertestcase@test.com",
+    password: "123123",
+  };
+
+  afterAll(async () => {
+    await User.deleteOne({ email: paramBody.email });
+    await mongoose.connection.close();
+  });
+
+  it("should return 200 status and success message after user creation", async () => {
+    const response = await request(app)
+      .post("/user/signupone")
+      .send(paramBody)
+      .expect(200);
+
+    expect(response.body.message).toBe("Account Created Successfully!");
+  });
+
+  it("should return 400 status if user already present in the DB", async () => {
+    const response = await request(app)
+      .post("/user/signupone")
+      .send(paramBody)
+      .expect(400);
+
+    expect(response.body.message).toBe("User Already Exists!");
+  });
+});
