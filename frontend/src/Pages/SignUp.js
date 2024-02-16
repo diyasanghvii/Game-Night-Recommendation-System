670058d961f8e932959dd79fe055c6bbc336a281
Sowmya <stanikon@asu.edu>
2024-02-16 12:27:47 -0700

    Created a stepper for the SignUp UI

--- frontend/src/Pages/SignUp.js
+++ frontend/src/Pages/SignUp.js
@@ -1,27 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
+import { Stepper, Step, StepLabel, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import SignUp1 from "../Components/Signup1/Signup1";
-import Signup2 from "../Components/Signup2/Signup2";
-import Signup3 from "../Components/Signup3/Signup3";
-
+import SignUp2 from "../Components/Signup2/Signup2";
+import SignUp3 from "../Components/Signup3/Signup3";
 
 const SignUp = () => {
-  let navigate = useNavigate();
+  const [activeStep, setActiveStep] = useState(0);
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    var storedAuthToken = sessionStorage.getItem("authToken");
-    if (storedAuthToken) {
-      navigate("/dashboard");
-    }
-  }, []);
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleFinish = () => {
+    navigate("/dashboard");
+  };
 
   return (
     <div>
       <h1>Sign Up Page</h1>
-      <h3>Add Your Signup Page Here</h3>
-      <SignUp1 />
-      <Signup2 />
-      <Signup3/>
+      <Stepper activeStep={activeStep} alternativeLabel>
+        <Step key={0}>
+          <StepLabel>Step 1</StepLabel>
+        </Step>
+        <Step key={1}>
+          <StepLabel>Step 2</StepLabel>
+        </Step>
+        <Step key={2}>
+          <StepLabel>Step 3</StepLabel>
+        </Step>
+      </Stepper>
+      <div>
+        {activeStep === 0 ? (
+          <SignUp1 />
+        ) : activeStep === 1 ? (
+          <SignUp2 />
+        ) : (
+          <SignUp3 />
+        )}
+      </div>
+      <div>
+        <Button disabled={activeStep === 0} onClick={handleBack}>
+          Back
+        </Button>
+        {activeStep === 2 ? (
+          <Button onClick={handleFinish}>Finish</Button>
+        ) : (
+          <Button onClick={handleNext}>Next</Button>
+        )}
+      </div>
     </div>
   );
 };
