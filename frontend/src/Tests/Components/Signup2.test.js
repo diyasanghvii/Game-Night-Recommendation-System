d8378c58f88b1a08066cbbd6880617a173659c65
nnolas33 <nnolas33@asu.edu>
2024-03-10 20:04:53 -0700

    Task #133 Wrote testcases for checking the UI handle

--- frontend/src/Tests/Components/Signup2.test.js
+++ frontend/src/Tests/Components/Signup2.test.js
@@ -1,6 +1,49 @@
 import React from "react";
-import { render, fireEvent,waitFor } from '@testing-library/react';
+import { render, fireEvent,waitFor,getByText } from '@testing-library/react';
 import Signup2 from "../../Components/Signup2/Signup2";
+
+describe("Signup2 component for verifying part", () => {
+  test("should disable continue button when Steam ID and Discord Username are not provided", async () => {
+    const { getByText } = render(<Signup2 />);
+    const continueButton = getByText("Continue");
+
+    expect(continueButton).toBeDisabled();
+  });
+
+  test("should disable continue button when Steam ID is not verified", async () => {
+    const { getByLabelText, getAllByText } = render(<Signup2 />);
+    const steamIdInput = getByLabelText("Steam ID");
+    const discordUsernameInput = getByLabelText("Discord Username");
+    const verifyBtns = getAllByText("Verify");
+
+    fireEvent.change(steamIdInput, { target: { value: "invalidSteamId" } });
+    fireEvent.change(discordUsernameInput, { target: { value: "naheerfatima_76086" } });
+    fireEvent.click(verifyBtns[0]); 
+  });
+
+  test("should disable continue button when Discord Username is not verified", async () => {
+    const { getByLabelText, getAllByText } = render(<Signup2 />);
+    const discordUsernameInput = getByLabelText("Discord Username");
+    const verifyBtns = getAllByText("Verify");
+
+    fireEvent.change(discordUsernameInput, { target: { value: "invalidDiscordUsername" } });
+    fireEvent.click(verifyBtns[1]); 
+  });
+
+  test("should enable continue button when both Steam ID and Discord Username are verified", async () => {
+    const { getByLabelText, getAllByText } = render(<Signup2 />);
+    const steamIdInput = getByLabelText("Steam ID");
+    const discordUsernameInput = getByLabelText("Discord Username");
+    const verifyBtns = getAllByText("Verify");
+
+    fireEvent.change(steamIdInput, { target: { value: "validSteamId" } });
+    fireEvent.change(discordUsernameInput, { target: { value: "validDiscordUsername" } });
+
+    fireEvent.click(verifyBtns[0]); 
+    fireEvent.click(verifyBtns[1]); 
+  });
+});
+
 
 describe('Signup2 component', () => {
   it('renders without crashing', () => {
