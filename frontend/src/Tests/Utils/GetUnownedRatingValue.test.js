c143232a291b17217e8a6f1ac40848e2539f9a2a
nnolas33 <nnolas33@asu.edu>
2024-04-03 14:34:00 -0700

    Task #203 Fixed testcase for GetOwnedRatingValue

--- frontend/src/Tests/Utils/GetUnownedRatingValue.test.js
+++ frontend/src/Tests/Utils/GetUnownedRatingValue.test.js
@@ -1,14 +1,14 @@
 import { INTERESTING, LOVE, MEH, getUnownedRatingValue } from "../../Utils";
 
 describe("getUnownedRatingValue", () => {
-  it('returns 1 when passed "interesting"', () => {
+  it('returns 0.75 when passed "interesting"', () => {
     const rating = getUnownedRatingValue("interesting");
-    expect(rating).toBe(LOVE);
+    expect(rating).toBe(INTERESTING);
   });
 
-  it('returns 0.75 when passed "love"', () => {
+  it('returns 1 when passed "love"', () => {
     const rating = getUnownedRatingValue("love");
-    expect(rating).toBe(INTERESTING);
+    expect(rating).toBe(LOVE);
   });
 
   it('returns 0 when passed "meh"', () => {
